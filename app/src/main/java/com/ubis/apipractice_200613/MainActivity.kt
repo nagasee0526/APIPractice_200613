873730fd3fafd4c1fb720b3def9d9a0e2af2ad82
nagasee0526 <roronalove@gmail.com>
2020-06-13 10:37:48 +0900

    API통신 Initial commit

---
+++ app/src/main/java/com/ubis/apipractice_200613/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ubis.apipractice_200613
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : baseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setEvents()
+        setValues()
+    }
+
+    override fun setValues() {
+    }
+
+    override fun setEvents() {
+    }
+
+}
---
+++ app/src/main/java/com/ubis/apipractice_200613/baseActivity.kt
@@ -0,0 +1,9 @@
+package com.ubis.apipractice_200613
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class baseActivity : AppCompatActivity() {
+
+    abstract fun setValues()
+    abstract fun setEvents()
+}
