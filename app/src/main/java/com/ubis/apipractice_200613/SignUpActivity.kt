fbf8412d2ae70e3cf9f459b09979d5975d5f8044
nagasee0526 <roronalove@gmail.com>
2020-06-13 13:44:56 +0900

    회원가입 액티비티 구현중

---
+++ app/src/main/java/com/ubis/apipractice_200613/SignUpActivity.kt
@@ -0,0 +1,22 @@
+package com.ubis.apipractice_200613
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : baseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        setEvents()
+        setValues()
+    }
+
+    override fun setValues() {
+    }
+
+    override fun setEvents() {
+    }
+}
