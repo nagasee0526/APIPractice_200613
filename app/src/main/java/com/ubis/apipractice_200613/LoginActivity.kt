cea3815aa3c8cd93d57f94553d6a46af08e2babb
nagasee0526 <roronalove@gmail.com>
2020-06-13 11:55:02 +0900

    서버에 로그인 실제요청기능 추가

--- app/src/main/java/com/ubis/apipractice_200613/LoginActivity.kt
+++ app/src/main/java/com/ubis/apipractice_200613/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.ubis.apipractice_200613
 
 import android.os.Bundle
+import com.ubis.apipractice_200613.utils.ServerUtils
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : baseActivity() {
@@ -22,6 +23,7 @@ class LoginActivity : baseActivity() {
             val inputEmail = emailEdt.text.toString()
             val inputPw = pwEdt.text.toString()
         // 실제로 서버에서 두개의 변수를 전달해서 로그인 시도
+        // 별개의 클래스 (ServerUtil)에 서버요철기능을 만들고 화면에서 이를 사용
 
 
         }
